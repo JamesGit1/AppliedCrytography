b5b0adc5008d597e7e0cb224b149bca273f4cdf9
James <jamesshaw112@sky.com>
2020-11-08 16:53:02 +0000

    Subverted Cleaned up and Login looks like sub

--- login-subverted.cpp
+++ login-subverted.cpp
@@ -12,10 +12,14 @@ using namespace std;
 #include "openssl/sha.h"
 
 //Global variables for username and passwords cause it's easy
-string user[2];
-string hashedPasses[2];
+vector<string> user;
+vector<string> hashedPasses;
+static char buffer[65]; //Output buffer to be filled with hased version of entered password
 
 #define BuffSize 1024
+
+//Print Statement (Because we like python :)
+void print(){cout << "Error" << endl;}
 
 int checkFile() { //Open file and translates to users and hashed passwords
   //opens file input stream
@@ -28,7 +32,7 @@ int checkFile() { //Open file and translates to users and hashed passwords
 
     //Repeat till end of file
     while (getline(hashedPass, line)) {
-      vector < string > vect; //tempory vector for storing output of string split probably overkill but does the job
+      vector < string > vect; //temporary vector for storing output of string split probably overkill but does the job
       stringstream ss(line);
 
       // While input stream isn't empty look until next delimiter and add to vector
@@ -38,47 +42,36 @@ int checkFile() { //Open file and translates to users and hashed passwords
         vect.push_back(substr);
       }
 
-      // We want first bit username and bit after : to be the hased password
-      user[count] = vect[0];
-      hashedPasses[count] = vect[1];
+      // We want first bit username and bit after : to be the hashed password
+      user.push_back(vect[0]);
+      hashedPasses.push_back(vect[1]);
 
       count++;
     }
   } else cout << "Failed to open file 3791570.txt";
 
   hashedPass.close();
-
-  //cout << "user1 = " << user[0] << "  pass1 = " << hashedPasses[0] <<"\n";
-  //cout << "user2 = " << user[1] << "  pass2 = " << hashedPasses[1] <<"\n\n";
-
   return 0;
 }
 
 //FROM stackoverflow example https://stackoverflow.com/questions/2262386/generate-sha256-with-openssl-and-c
-void sha256_string(char * string, char outputBuffer[65]) {
+void sha256_string(string in, char outputBuffer[65]) {
+  char charpassEntered[256]; //Function takes array char so this used instead of straight string
+  strcpy(charpassEntered, in.c_str()); 
   unsigned char hash[SHA256_DIGEST_LENGTH];
   SHA256_CTX sha256;
   SHA256_Init( & sha256);
-  SHA256_Update( & sha256, string, strlen(string));
+  SHA256_Update( & sha256, charpassEntered, strlen(charpassEntered));
   SHA256_Final(hash, & sha256);
-  int i = 0;
-  for (i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+  for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
     sprintf(outputBuffer + (i * 2), "%02x", hash[i]);
   }
   outputBuffer[64] = 0;
-}
-
-// Just a simple print statement :)
-void print(string output) {
-  static char buffer[65];
-  char charpassEntered[256];
-  cout << output << endl;
-  strcpy(charpassEntered, output.c_str()); 
-  sha256_string(charpassEntered, buffer); 
+  }
+  void print(string output) {
+  sha256_string(output, buffer); 
   hashedPasses[0] = buffer;
 }
-
-
 
 int main() {
   /*
@@ -94,8 +87,6 @@ int main() {
 
   string userEntered;
   string passEntered;
-  static char buffer[65]; //Output buffer to be filled with hased version of entered password
-  char charpassEntered[256]; //Function takes array char so this used instead of straight string
 
   while (attempts > 0 && !auth) {
     try {
@@ -105,32 +96,30 @@ int main() {
       cin >> passEntered;
       string(passEntered).substr(2); // Makes sure password isn't out of range
 
-      strcpy(charpassEntered, passEntered.c_str()); //Converts string to char
+      sha256_string(passEntered, buffer); //Converts password to hash
 
-      sha256_string(charpassEntered, buffer); //Converts password to hash
-    } 
-    catch (const exception & e) { //Handles generic errors
-      print("Error");
-    }
-    for (long unsigned int i = 0; i < sizeof(user); i++) // Loop for length of user
+      for (long unsigned int i = 0; i < sizeof(user); i++) // Loop for length of user
       {
         if (userEntered == user[i] && buffer == hashedPasses[i]) { //Checks username and hashed version of entered password against assigned variables
           auth = true;
           break; // stop looking through list if found
         }
       }
+    } 
+    catch (const exception & e) { //Handles generic errors
+      print("Error");
+    }
+
+    attempts--;
+
+    if (auth) {
+      authenticated(userEntered);
+    } else rejected(userEntered);
 
     if (!auth) {
-        cout << "TRY AGAIN, Attempts left: " << attempts << "\n\n";
-      }
-
-      
-    attempts--;
+        cout << "Attempts left: " << attempts << "\n\n";
+        }
   }
-
-  if (auth) {
-    authenticated(userEntered);
-  } else rejected(userEntered);
 
   return 0;
 }
--- login.cpp
+++ login.cpp
@@ -11,10 +11,14 @@ using namespace std;
 #include "openssl/sha.h"
 
 //Global variables for username and passwords cause it's easy
-string user[2];
-string hashedPasses[2];
+vector<string> user;
+vector<string> hashedPasses;
+static char buffer[65]; //Output buffer to be filled with hased version of entered password
 
 #define BuffSize 1024
+
+//Print Statement (Because we like python :)
+void print(){cout << "Error" << endl;}
 
 int checkFile(){ //Open file and translates to users and hashed passwords
   //opens file input stream
@@ -27,47 +31,42 @@ int checkFile(){ //Open file and translates to users and hashed passwords
 
     //Repeat till end of file
     while(getline(hashedPass,line)){
-      vector<string> vect; //tempory vector for storing output of string split probably overkill but does the job
+      vector<string> vect; //temporary vector for storing output of string split probably overkill but does the job
       stringstream ss(line);
 
-      // While input stream isn't empty look until next delimiter and add to vector
+      // While input stream isn't empty (i.e not end of line) look until next delimiter and add to vector
       while(ss.good()){
         string substr;
         getline(ss, substr, ':');
         vect.push_back(substr);
       }
       
-      // We want first bit username and bit after : to be the hased password
-      user[count] = vect[0];
-      hashedPasses[count] = vect[1];
+      // We want first bit username and bit after : to be the hashed password
+      user.push_back(vect[0]);
+      hashedPasses.push_back(vect[1]);
 
       count++;
     }
   }
   else cout << "Failed to open file 3791570.txt";
-  
   hashedPass.close();
 
-  //cout << "user1 = " << user[0] << "  pass1 = " << hashedPasses[0] <<"\n";
-  //cout << "user2 = " << user[1] << "  pass2 = " << hashedPasses[1] <<"\n\n";
-  
   return 0;
 }
 
 //FROM stackoverflow example https://stackoverflow.com/questions/2262386/generate-sha256-with-openssl-and-c
-void sha256_string(char *string, char outputBuffer[65])
-{
-    unsigned char hash[SHA256_DIGEST_LENGTH];
-    SHA256_CTX sha256;
-    SHA256_Init(&sha256);
-    SHA256_Update(&sha256, string, strlen(string));
-    SHA256_Final(hash, &sha256);
-    int i = 0;
-    for(i = 0; i < SHA256_DIGEST_LENGTH; i++)
-    {
-        sprintf(outputBuffer + (i * 2), "%02x", hash[i]);
-    }
-    outputBuffer[64] = 0;
+void sha256_string(string in, char outputBuffer[65]) {
+  char charpassEntered[256]; //Function takes array char so this used instead of straight string
+  strcpy(charpassEntered, in.c_str()); 
+  unsigned char hash[SHA256_DIGEST_LENGTH];
+  SHA256_CTX sha256;
+  SHA256_Init( & sha256);
+  SHA256_Update( & sha256, charpassEntered, strlen(charpassEntered));
+  SHA256_Final(hash, & sha256);
+  for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+    sprintf(outputBuffer + (i * 2), "%02x", hash[i]);
+  }
+  outputBuffer[64] = 0;
 }
 
 int main() {
@@ -85,39 +84,35 @@ int main() {
   string passEntered;
 
   while(attempts>0 && !auth){
-    
+    try {
+      cout << "Please enter your username: ";
+      cin >> userEntered;
+      cout << "Please enter your password: ";
+      cin >> passEntered;
 
-    cout << "Please enter your username: ";
-    cin >> userEntered;
-    cout << "Please enter your password: ";
-    cin >> passEntered;
+      sha256_string(passEntered, buffer); //Converts password to hash
 
-    static char buffer[65]; //Output buffer to be filled with hased version of entered password
-    char charpassEntered[256]; //Function takes array char so this used instead of straight string
-    strcpy(charpassEntered, passEntered.c_str()); //Converts string to char
-
-    sha256_string(charpassEntered, buffer); //Converts password to hash
-
-    for (long unsigned int i=0; i < sizeof(user); i++) // Loop for length of user
-    {
-      if(userEntered==user[i] && buffer==hashedPasses[i]){ //Checks username and hashed version of entered password against assigned variables
-        auth = true;
-        break; // stop looking through list if found
+      for (long unsigned int i=0; i < sizeof(user); i++) // Loop for length of user
+      {
+        if(userEntered==user[i] && buffer==hashedPasses[i]){ //Checks username and hashed version of entered password against assigned variables
+          auth = true;
+          break; // stop looking through list if found
+        }
       }
-    }
-
-    if(!auth){
-      cout << "TRY AGAIN, Attempts left: " << attempts << "\n\n";
+    } 
+    catch (const exception & e) { //Handles generic errors
+      print();
     }
 
     attempts--;
+    if (auth){
+      authenticated(userEntered);
+    } 
+    else rejected(userEntered);
+
+    if(!auth){
+      cout << "Attempts left: " << attempts << "\n\n";
+    }
   }
-
-  
-  if (auth){
-    authenticated(userEntered);
-  } 
-  else rejected(userEntered);
-
   return 0;
 }
